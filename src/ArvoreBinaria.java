c44efd78a16db7709af2e9256f0d75807f761993
Juliocesar043 <julio.cesarl@edu.unifil.br>
2026-05-19 02:28:58 -0300

    Implementação recursiva do método Remover

---
+++ src/ArvoreBinaria.java
@@ -0,0 +1,174 @@
+public class ArvoreBinaria {
+    No raiz;
+
+    public ArvoreBinaria() {
+        this.raiz = new No(null);
+        System.out.println("Árvore Binária criada com sucesso!");
+    }
+
+//    public void inserir(Integer conteudo) {
+//        No novoNo = new No(conteudo);
+//
+//        if(estaVazia()) {
+//            this.raiz = novoNo;
+//        } else {
+//            No aux = this.raiz;
+//            while(true) {
+//                if (aux.getConteudo() > novoNo.getConteudo()) {
+//                    if (aux.getEsquerda() == null) {
+//                        aux.setEsquerda(novoNo);
+//                        return;
+//                    } else {
+//                        aux = aux.getEsquerda();
+//                    }
+//                } else if (aux.getConteudo() == novoNo.getConteudo()) {
+//                    System.out.println("Não é possível informar nós repetidos.");
+//                    return;
+//                } else {
+//                    if (aux.getDireita() == null) {
+//                        aux.setDireita(novoNo);
+//                        return;
+//                    } else {
+//                        aux = aux.getDireita();
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+    public void inserir(Integer conteudo) {
+        No novoNo = new No(conteudo);
+
+        if(estaVazia()) {
+            this.raiz = novoNo;
+        } else {
+            inserirRecursivo(novoNo, this.raiz);
+        }
+    }
+
+    public void inserirRecursivo(No novoNo, No atual) {
+        if (atual.getConteudo() > novoNo.getConteudo()) {
+            if (atual.getEsquerda() == null) {
+                atual.setEsquerda(novoNo);
+                System.out.println("O nó " + novoNo.getConteudo() + " foi inserido na Árvore.");
+                return;
+            } else {
+                inserirRecursivo(novoNo, atual.getEsquerda());
+            }
+        } else if (atual.getConteudo() == novoNo.getConteudo()) {
+            System.out.println("Não é possível informar nós repetidos.");
+            return;
+        } else {
+            if (atual.getDireita() == null) {
+                atual.setDireita(novoNo);
+                System.out.println("O nó " + novoNo.getConteudo() + " foi inserido na Árvore.");
+                return;
+            } else {
+                inserirRecursivo(novoNo, atual.getDireita());
+            }
+        }
+    }
+
+    public No buscarConteudo(Integer conteudo, No atual) {
+
+        if (atual == null) {
+            System.out.println("Conteúdo não encontrado.");
+            return null;
+        }
+
+        if (conteudo > atual.getConteudo()) {
+            atual.setDireita(buscarConteudo(conteudo, atual.getDireita()));
+
+        } else if (conteudo < atual.getConteudo()) {
+            atual.setEsquerda(buscarConteudo(conteudo, atual.getEsquerda()));
+
+        } else if (conteudo == atual.getConteudo()) {
+            System.out.println("Conteúdo encontrado!");
+            atual = removerNo(atual);
+            return atual;
+        }
+        return atual;
+    }
+
+    public void remover(Integer conteudo) {
+        this.raiz = buscarConteudo(conteudo, this.raiz);
+    }
+
+    private No removerNo(No atual) {
+        // Nó folha
+        if(atual.getDireita() == null && atual.getEsquerda() == null) {
+            return null;
+        } // Nó com filho à esquerda
+        else if(atual.getDireita() == null){
+            return atual.getEsquerda();
+        } // Nó com filho à direita
+        else if(atual.getEsquerda() == null) {
+            return atual.getDireita();
+        } // Nó com dois filhos
+        else {
+            No sucessor = atual.getDireita();
+            while (sucessor.getEsquerda() != null) {
+                sucessor = sucessor.getEsquerda();
+            }
+            Integer valorSucessor = sucessor.getConteudo();
+            atual.setConteudo((valorSucessor));
+
+            No direitaAntes = atual.getDireita();
+
+            No direitaDepois = buscarConteudo(valorSucessor, direitaAntes);
+            atual.setDireita(direitaDepois);
+            return atual;
+        }
+    }
+
+    public boolean estaVazia() {
+        if(this.raiz.getConteudo() == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void preOrdem(No no) {
+        if(no == null) {
+            return;
+        }
+        System.out.println(no.getConteudo());
+        preOrdem(no.getEsquerda());
+        preOrdem(no.getDireita());
+    }
+
+    private void emOrdem(No no) {
+        if(no == null) {
+            return;
+        }
+        emOrdem(no.getEsquerda());
+        System.out.println(no.getConteudo());
+        emOrdem(no.getDireita());
+    }
+
+    private void posOrdem(No no) {
+        if(no == null) {
+            return;
+        }
+        posOrdem(no.getEsquerda());
+        posOrdem(no.getDireita());
+        System.out.println(no.getConteudo());
+    }
+
+    public void exibir(String percurso) {
+        switch (percurso){
+            case("Pre"):
+                preOrdem(this.raiz);
+                break;
+            case("Em"):
+                emOrdem(this.raiz);
+                break;
+            case("Pos"):
+                posOrdem(this.raiz);
+                break;
+        }
+    }
+
+
+}
